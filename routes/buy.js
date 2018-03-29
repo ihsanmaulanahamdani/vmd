fc5551fa867a3fe4ca53ceb432918b4e3e509211
ihsanmaulanahamdani <ihsanmaulana5@gmail.com>
2018-03-29 10:36:33 +0700

    fixing_relation_login_and_customer_page

--- routes/buy.js
+++ routes/buy.js
@@ -47,44 +47,5 @@ router.get('/:id', (req, res) => {
     res.send(err.message)
   })
 })
-//
-// // edit data item
-// router.post('/edit/:id',function(req,res){
-//   // console.log(req.params)
-//   let idObj = req.params.id
-//   let obj = {
-//     name: req.body.name,
-//     stock: req.body.stock,
-//     price: req.body.price,
-//     createdAt: new Date(),
-//     updatedAt: new Date()
-//   }
-//   // console.log('==============',obj)
-//   model.Item.update(obj, {
-//       where:{id:idObj}
-//   })
-//   .then(() => {
-//     res.redirect('/items')
-//   })
-//   .catch(err => {
-//     res.send('ini errrrrrr',err)
-//   })
-// })
-//
-// // delete item
-// router.get('/delete/:id', (req, res) => {
-//   let idObj = req.params.id
-//   model.Item.destroy({
-//     where: {
-//       id: idObj
-//     }
-//   })
-//   .then(() => {
-//     res.redirect('/items')
-//   })
-//   .catch(err => {res.send(err)})
-// })
-
-
 
 module.exports = router
