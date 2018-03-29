7bd713eeadd6774df448d282460b5a49e575b9a2
ihsanmaulanahamdani <ihsanmaulana5@gmail.com>
2018-03-29 10:14:45 +0700

    fixing customer feature and add history page

--- routes/customer.js
+++ routes/customer.js
@@ -26,4 +26,51 @@ router.get('/edit/:id', (req, res) => {
     })
 })
 
+router.post('/edit/:id', (req, res) => {
+    db.Customer.findOne({
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(customer => {
+            db.Customer.update({
+                    parent_name: req.body.parent_name,
+                    email: req.body.email,
+                    password: req.body.password,
+                    salt: req.body.salt,
+                    child_name: req.body.child_name,
+                    pin: req.body.pin,
+                    saldo: req.body.saldo,
+                    budget_limit: req.body.budget_limit,
+                    
+                }, {
+                    where: {
+                        id: req.params.id
+                    }
+                })
+                .then(data => {
+                    res.redirect('/customers')
+                })
+                .catch(err => {
+                    res.send(err.message)
+                })
+        })
+        
+
+})
+
+router.get('/delete/:id', (req, res) => {
+    db.Customer.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(data => {
+            res.redirect('/customers')
+        })
+        .catch(err => {
+            res.send(err.message)
+        })
+})
+
 module.exports = router;
